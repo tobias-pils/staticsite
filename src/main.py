42a1f86539384530b0c23db5b4381f26cb257edd
Tobias Pils <tobias.pils@proton.me>
2025-03-19 19:06:49 +0100

    implement copying of static files

--- src/main.py
+++ src/main.py
@@ -1,5 +1,31 @@
+import os
+import shutil
+
+STATIC_PATH = "static"
+PUBLIC_PATH = "public"
+
+def clear_public_dir():
+    shutil.rmtree(PUBLIC_PATH)
+
+def copy_static_files(sub_path=""):
+    path = os.path.join(STATIC_PATH, sub_path)
+    if os.path.exists(path):
+        filenames = os.listdir(path)
+        for filename in filenames:
+            file_path = os.path.join(path, filename)
+            if os.path.isdir(file_path):
+                copy_static_files(os.path.join(sub_path, filename))
+            else:
+                dest_dir = os.path.join(PUBLIC_PATH, sub_path)
+                if not os.path.exists(dest_dir):
+                    print(f"make dir {dest_dir}")
+                    os.mkdir(dest_dir)
+                print(f"copy file {filename}")
+                shutil.copy(file_path, os.path.join(dest_dir, filename))
+
 def main():
-    print("hello world")
+    clear_public_dir()
+    copy_static_files()
 
 if __name__ == "__main__":
     main()
